b9f56980d0aa07015239b91bdc24f5acf55f3367
KyleN29 <gcarey0@outlook.com>
2025-01-02 21:28:49 -0600

    added node backend

---
+++ server.cjs
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require("path");
+const app = express()
+const port = 10000
+
+const buildPath = path.normalize(path.join(__dirname, './dist'));
+
+app.use(express.static(__dirname + '/dist'))
+
+// app.get('/', (req, res) => {
+//     res.sendFile('views/main.html', {root: __dirname })
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
